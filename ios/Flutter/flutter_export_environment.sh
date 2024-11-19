7a0cfff43c658379c813b69b5cea462fabd7b454
Arnav Ajay <arnavajay@Arnavs-MacBook-Pro-3.local>
2024-11-18 23:38:44 -0500

    bruh

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\darre\VSCode Projects\Students Who Sit\SWS-Sprint_1\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\darre\VSCode Projects\FishBox\fishboxapp"
+export "FLUTTER_ROOT=/Users/arnavajay/Documents/FishBox/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/arnavajay/Documents/FishBox/fishboxapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=/Users/arnavajay/Documents/FishBox/fishboxapp/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/arnavajay/Documents/FishBox/fishboxapp/.dart_tool/package_config.json"
